719b1d6717bc02be36210451b2f88bda1207dbd3
USB870 <99743348+USB870@users.noreply.github.com>
2022-03-27 20:45:52 +0200

    Update web_scraping.py

--- web_scraping.py
+++ web_scraping.py
@@ -1,7 +1,8 @@
 import requests, bs4, webbrowser
-
 cost_link = 'https://www.staseraintv.com/programmi_stasera_'
 possibili_canali = ['canale5.html#pal', 'italia1.html#pal', 'canale20mediaset.html#pal', 'rai4.html#pal', 'iris.html#pal']
+canali_numero = ['CANALE 5', 'CANALE 6', 'CANALE 20', 'CANALE 21', 'CANALE 22']
+
 for i in range(0,5):
     attuale_possibili_canali = possibili_canali[i]
     link = cost_link+attuale_possibili_canali
@@ -15,5 +16,12 @@ for i in range(0,5):
         link_sezione_new = str(a_sezione_new.get('href'))
         if link_sezione_new not in link_sezione:
             link_sezione.append(link_sezione_new)
-    print(link_sezione)
-    print('\n')
+    penultimo = link_sezione[-2]
+    penultimo_splittato = penultimo.split('/')
+    ultimo = penultimo_splittato[-1]
+    penultimo_splittato = ultimo.split('.html')
+    penultimo_splittato.pop()
+    valore_finale = penultimo_splittato[0]
+    print(canali_numero[i]+'\n')
+    print(valore_finale)
+    print('############################################\n')
